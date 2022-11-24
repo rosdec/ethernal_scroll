8d81ac558c22c7b68d20dbb7edee42a3dface3bb
Rosario De Chiara <rosario.dechiara@posteitaliane.it>
2022-11-24 18:32:53 +0100

    ** UP ** Changed logic

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <main>
-    <div id="app">
-      <h1>Ethernal scroll of Random users</h1>
-      <div id="avatars">
-        <div class="user" v-for="user in users" :key="user.first_name">
-          <div class="user-avatar">
-            <img :src="user.avatar" />
-          </div>
-          <div class="user-details">
-            <h2 class="user-name">
-              {{ user.first_name }}
-              {{ user.last_name }}
-            </h2>
-            <ul>
-              <li><strong>Birthday:</strong> {{ formatDate(user.date_of_birth) }}</li>
-              <li><strong>Location:</strong> {{ user.address.city }}, {{ user.address.state }}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+
+    <div class="heading">
+      <h1>Infinite Scroll</h1>
+      <h4>A simple infinite scroll example using Vue.js</h4>
     </div>
+
+    <div class="container" id="app">
+
+      <div class="list-group-wrapper">
+        <transition name="fade">
+          <div class="loading" v-show="loading">
+            <span class="fa fa-spinner fa-spin"></span> Loading
+          </div>
+        </transition>
+        <ul class="list-group" id="infinite-list">
+          <li class="list-group-item" v-for="item in items" v-text="item"></li>
+        </ul>
+      </div>
+
+    </div>
+
   </main>
 </template>
 
@@ -29,71 +30,40 @@ import axios from "axios";
 export default {
   data() {
     return {
-      users: [],
+      loading: false,
+      items: []
     };
   },
   methods: {
-    getInitialUsers() {
-      axios.get(`https://random-data-api.com/api/v2/users?size=6&response_type=json`).then((response) => {
-        this.users = response.data;
-        console.log(response.data)
+    getNextBatch(size) {
+      this.loading = true;
+      axios.get("https://random-data-api.com/api/v2/users?size=" + size + "&response_type=json").then((response) => {
+        
+        response.data.map((x) => {
+          console.log(x.first_name)
+          this.items.push(x.first_name + " " + x.last_name) 
+        })
+        this.loading = false;
       });
-    },
-    formatDate(dateString) {
-      let convertedDate = new Date(dateString);
-      return convertedDate.toDateString();
-    },
-    getNextUser() {
-      window.onscroll = () => {
-        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
-        if (bottomOfWindow) {
-          axios.get(`https://random-data-api.com/api/v2/users?size=2&response_type=json`).then(response => {
-            this.users.push(response.data[0]);
-            this.users.push(response.data[1]);
-          });
-        }
-      }
-    },
+    }
   },
   beforeMount() {
-    this.getInitialUsers();
+    this.getNextBatch(30);
   },
   mounted() {
-    this.getNextUser();
+    const listElm = document.querySelector('#infinite-list');
+
+    listElm.addEventListener('scroll', e => {
+      if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+        this.getNextBatch(2);
+      }
+    })
   },
 };
 </script>
 
 <style>
-.user {
-  display: flex;
-  background: #ccc;
-  border-radius: 0.5em;
-  margin: 1em auto;
-}
 
-.user-avatar {
-  padding: 1em;
-}
-
-.user-avatar img {
-  display: block;
-  width: 60%;
-  min-width: 64px;
-  height: auto;
-  border-radius: 50.5em;
-}
-
-.user-details {
-  padding: 1em;
-}
-
-.user-name {
-  margin: 0;
-  padding: 0;
-  font-size: 1rem;
-  font-weight: 900;
-}
 </style>
 
 
